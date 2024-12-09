33164e38e538a5a236de1e7a16a7b4ba96a5d492
tzlan <talzana55@gmail.com>
2024-12-09 10:43:27 +0200

    Fix name App /Home

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import Home from './pages/Home.tsx'
+import './pages/App.tsx'
+
+import App from './pages/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home/>
+    <App/>
       
   
  
